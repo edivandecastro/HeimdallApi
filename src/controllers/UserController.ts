1f5357d8ed7411c0acbd9d13590fc292341cdbf4
Edivan de Castro <edivandecastro@gmail.com>
2020-09-12 17:06:51 -0300

    Adicionado controllers para o usuário

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,37 @@
+import { Request, Response } from 'express'
+import User from '../schemas/User'
+
+class UserController {
+  public async create(req: Request, res: Response) {
+    let { user } = req.body;
+
+    await User.create(user, (err: any, user: any) => {
+      if (err) {
+        let reason = err.errmsg
+
+        if (err.code == "11000")
+          reason = "Username already exists!";
+
+        res.status(400).send({ "message": "There was an error registering the user!", "reason": reason });
+      }
+      else {
+        res.status(200).send({ "message": "User registered with success!", user_id: user.id });
+      }
+    });
+  }
+
+  public async show(req: Request, res: Response) {
+    let id = req.params.id;
+
+    await User.findById(id, (err, user) => {
+      if(user) {
+        return res.json(user);
+      }
+      else {
+        return res.json({ "message": "Não foi encontrado usuário para o parametro informado." });
+      }
+    });
+  }
+}
+
+export default new UserController
